a333f48ec8a3e8cf8154869e6e0a07394c2266ef
Mbabazi Elizabeth Shirley <52815955+Shirley-Liz@users.noreply.github.com>
2023-07-07 03:54:16 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+﻿const ball1 = document.getElementById("ball1");
+const ball2 = document.getElementById("ball2");
+const ball3 = document.getElementById("ball3");
+const ball4 = document.getElementById("ball4");
+
+ball1.style.animation = "orbit-outer 4s infinite linear -1s";
+ball2.style.animation = "orbit-outer 4s infinite linear -2s";
+ball3.style.animation = "orbit-inner 4s infinite linear -3s";
+ball4.style.animation = "orbit-inner 4s infinite linear -4s";
